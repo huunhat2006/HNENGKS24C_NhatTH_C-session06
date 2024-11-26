95d6ec797e32c1bb93a48a894eb372075461c9b3
Huunhat123 <huunhat20052006@gmail.com>
2024-11-26 16:48:38 +0700

    session06-03

---
+++ session06--03.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int mk =200506;
+	int num;	 
+    
+	while(num != mk){
+	printf("Moi ban nhap mat khau:");
+	scanf("%d",&num);
+			if(num!=mk){
+		printf("ban nhap sai roi\n"); 
+	}
+	else{
+		printf("ban nhap dung roi\n"); 
+	} 
+	}
+
+	
+	
+	return 0;
+
+ }
+
